977c65c7f76caebde7f798fa2304063f64aa3d93
Anwesh43 <anweshthecool0@gmail.com>
2018-08-02 02:50:03 +0530

    adding SlackLineStage class

---
+++ SlackLineStage.ts
@@ -0,0 +1,33 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+class SlackLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+
+    context : CanvasRenderingContext2D
+    constructor() {
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage = new SlackLineStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
